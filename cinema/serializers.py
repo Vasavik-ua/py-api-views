3ba97a9f624c21428198c1dcd9a6259b5a69c0ac
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-03-01 22:49:38 +0100

    fix problem with identations

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -12,14 +12,17 @@ class ActorSerializer(serializers.Serializer):
         return Actor.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.first_name = validated_data.get(
+            "first_name", instance.first_name
+        )
+        instance.last_name = validated_data.get(
+            "last_name", instance.last_name
+        )
         instance.save()
         return instance
 
 
-
- class GenreSerializer(serializers.Serializer):
+class GenreSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255, required=True)
 
@@ -27,7 +30,7 @@ class ActorSerializer(serializers.Serializer):
         return Genre.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
+        instance.name = validated_data.get("name", instance.name)
         instance.save()
         return instance
 
@@ -42,12 +45,13 @@ class CinemaHallSerializer(serializers.Serializer):
         return CinemaHall.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.rows = validated_data.get('rows', instance.rows)
-        instance.seats_in_row = validated_data.get('seats_in_row', instance.seats_in_row)
+        instance.name = validated_data.get("name", instance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row", instance.seats_in_row
+        )
         instance.save()
         return instance
-
 
 
 class MovieSerializer(serializers.Serializer):
