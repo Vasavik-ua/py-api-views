6b0431b95f1e57a28a80f74ba6dff7102bf64add
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-03-01 22:15:52 +0100

    add new enadpoint in urlpatterns for genre, actor and Cinemahall views

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,29 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import movie_list, movie_detail, CinemaHallViewSet, GenreList, GenreDetail, ActorList, ActorDetail
+
+cinema_hall_list = CinemaHallViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create",
+    }
+)
+cinema_hall_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
+)
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genre/", GenreList.as_view(), name="genre-list"),
+    path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actor/", ActorList.as_view(), name="actor-list"),
+    path("actor/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinemahall/", cinema_hall_list, name="cinemahall-list"),
+    path("cinemahall/<int:pk>/", cinema_hall_detail, name="cinemahall-detail")
 ]
 
 app_name = "cinema"
