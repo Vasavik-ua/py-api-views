29588fd95cf0da3bb62a932fc5aa392eedfbaf76
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-03-02 23:08:55 +0100

    fix names of path

--- cinema/urls.py
+++ cinema/urls.py
@@ -26,16 +26,16 @@ cinema_hall_detail = CinemaHallViewSet.as_view(
     }
 )
 router = routers.DefaultRouter()
-router.register("movie", MovieViewSet)
+router.register("movies", MovieViewSet)
 
 urlpatterns = [
-    path("genre/", GenreList.as_view(), name="genre-list"),
-    path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
-    path("actor/", ActorList.as_view(), name="actor-list"),
-    path("actor/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("cinemahall/", cinema_hall_list, name="cinemahall-list"),
-    path("cinemahall/<int:pk>/", cinema_hall_detail, name="cinemahall-detail"),
-    path("", include(router.urls)),
+    path("genres/", GenreList.as_view(), name="genre-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
+    path("cinema_halls/", cinema_hall_list, name="cinemahall-list"),
+    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinemahall-detail"),
+    path("", include(router.urls), name="movie"),
 ]
 
 app_name = "cinema"
