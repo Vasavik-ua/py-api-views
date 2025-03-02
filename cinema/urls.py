e505987c03e1eb01b331e908c7669afc9bad8171
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-03-02 23:11:47 +0100

    fix flake8

--- cinema/urls.py
+++ cinema/urls.py
@@ -34,7 +34,10 @@ urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinemahall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinemahall-detail"),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinemahall-detail"),
     path("", include(router.urls), name="movie"),
 ]
 
