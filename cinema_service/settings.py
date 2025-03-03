304a1b15e3a6e4ef28754203984da4ab5b384345
Vasyl Kalish <vasyl.documents@yahoo.it>
2025-03-03 19:59:40 +0100

    fix secret key in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ("django-insecure-ru6ndfka@95_"
-              "(lysua8yhdjq@vpiqgv3yru4r)q3h4_u8x7dfy")
+SECRET_KEY = os.environ.get(
+    "DJANGO_SECRET_KEY",
+    "django-insecure-2e(_&y)fxu@+8-_vg5h"
+    "(bg&$-oz-lhdcv*67cg$is%5q)3u@u_")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
